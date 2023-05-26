cf321fab89d85fe05e2e9e1ed83401d666e66774
javokhirergashev <iqtidorziyosi@gmail.com>
2023-05-26 23:39:35 +0500

    category ui

--- backend/views/category/create.php
+++ backend/views/category/create.php
@@ -5,13 +5,23 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\Category $model */
 
-$this->title = 'Create Category';
+$this->title = 'Yangi kategoriya yaratish';
 $this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="category-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['site/index']) ?>">Dashboard </a></li>
+                    <li class="breadcrumb-item"><i class="feather-chevron-right"></i></li>
+                    <li class="breadcrumb-item active"><?= Html::encode($this->title) ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <h3><?= Html::encode($this->title) ?></h3>
 
     <?= $this->render('_form', [
         'model' => $model,

5703e46a00270155289c32f0f7595968e84d66a2
javokhirergashev <iqtidorziyosi@gmail.com>
2023-05-27 21:47:03 +0500

    admin ui

--- backend/views/position/_form.php
+++ backend/views/position/_form.php
@@ -12,19 +12,49 @@ use yii\widgets\ActiveForm;
         <?php $form = ActiveForm::begin(); ?>
         <div class="col-md-12">
             <div class="card-box">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <?php $languages = ['uz', 'en', 'ru'] ?>
+
+                    <?php foreach ($languages as $key => $language): ?>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link <?= $key != 0 ? ' ' : 'active' ?>"
+                                    id="<?= $language ?>-tab"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#<?= $language ?>" type="button" role="tab"
+                                    aria-controls="<?= $language ?>"
+                                    aria-selected="true"><?= $language ?>
+                            </button>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
                 <form action="#">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group row">
-                                <label class="col-md-3 col-form-label">Nomi</label>
-                                <div class="col-md-9">
-                                    <?= $form->field($model, 'title')->textarea(['rows' => 6])->label(false) ?>
+                                <div class="form-group row">
+                                    <div class="tab-content" id="myTabContent">
+                                        <?php foreach ($languages
+                                                       as $key => $language): ?>
+                                            <div class="tab-pane <?= $key != 0 ? ' ' : 'show active' ?>"
+                                                 id="<?= $language ?>" role="tabpanel"
+                                                 aria-labelledby="<?= $language ?>-tab">
+                                                <div class="col-md-12">
+                                                    <div class="form-group row" style="margin-right: 0px!important; margin-bottom: 0px!important;">
+                                                        <label class="col-form-label">Nomi</label>
+                                                        <div class="col-md-9" style="margin-right: 0px!important;">
+                                                            <?= $form->field($model, 'title[' . $language . ']')->textInput(['maxlength' => true])->label(false) ?>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="form-group row">
-                                <label class="col-md-3 col-form-label">Statusi</label>
+                            <div class="form-group row" style="padding-top: 20px!important;">
+                                <label class="col-form-label">Statusi</label>
                                 <div class="col-md-9">
                                     <?= $form->field($model, 'status')->textInput()->label(false) ?>
                                 </div>
@@ -33,7 +63,7 @@ use yii\widgets\ActiveForm;
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group row">
-                                    <label class="col-md-3 col-form-label">Lavozim turi</label>
+                                    <label class="col-form-label">Lavozim turi</label>
                                     <div class="col-md-9">
                                         <?= $form->field($model, 'type')->textInput()->label(false) ?>
                                     </div>

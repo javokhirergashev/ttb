2970a00656454244109ef2fbc9dce6c2c410ce48
bortiqov <ortiqovbahodir1902@gmail.com>
2023-06-14 22:01:26 +0500

    fix login

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -17,7 +17,7 @@ $this->title = 'Login';
             <div class="col-lg-6 login-wrap">
                 <div class="login-sec">
                     <div class="log-img">
-                        <img class="img-fluid"  src="/backend-files/img/login-02.png" alt="Logo">
+                        <img class="img-fluid" src="/backend-files/img/login-02.png" alt="Logo">
                     </div>
                 </div>
             </div>
@@ -27,33 +27,29 @@ $this->title = 'Login';
                         <div class="login-right">
                             <div class="login-right-wrap">
                                 <div class="account-logo">
-                                    <a href="index.html"><img  src="/backend-files/img/login-logo.png" alt=""></a>
+                                    <a href="index.html"><img src="/backend-files/img/login-logo.png" alt=""></a>
                                 </div>
                                 <h2>Login</h2>
-
-                                <form action="">
-                                    <div class="form-group">
-                                        <label>Username <span class="login-danger">*</span></label>
-                                        <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label(false) ?>
+                                <div class="form-group">
+                                    <label>Username <span class="login-danger">*</span></label>
+                                    <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label(false) ?>
+                                </div>
+                                <div class="form-group">
+                                    <label>Password <span class="login-danger">*</span></label>
+                                    <?= $form->field($model, 'password')->passwordInput()->label(false) ?>
+                                    <span class="profile-views feather-eye-off toggle-password"></span>
+                                </div>
+                                <div class="forgotpass">
+                                    <div class="remember-me">
+                                        <label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
+                                            <?= $form->field($model, 'rememberMe')->checkbox()->label(false) ?>
+                                            <span class="checkmark"></span>
+                                        </label>
                                     </div>
-                                    <div class="form-group">
-                                        <label>Password <span class="login-danger">*</span></label>
-                                        <?= $form->field($model, 'password')->passwordInput()->label(false) ?>
-                                        <span class="profile-views feather-eye-off toggle-password"></span>
-                                    </div>
-                                    <div class="forgotpass">
-                                        <div class="remember-me">
-                                            <label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
-                                                <?= $form->field($model, 'rememberMe')->checkbox()->label(false) ?>
-                                                <span class="checkmark"></span>
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group login-btn">
-                                        <button class="btn btn-primary btn-block" type="submit">Login</button>
-                                    </div>
-                                </form>
-
+                                </div>
+                                <div class="form-group login-btn">
+                                    <button class="btn btn-primary btn-block" type="submit">Login</button>
+                                </div>
                             </div>
                         </div>
                     </div>

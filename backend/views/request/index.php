dde6d328e4bb66c28484b45b3293da91b667e7e8
javokhirergashev <iqtidorziyosi@gmail.com>
2023-06-15 16:49:24 +0500

    request_view

--- backend/views/request/index.php
+++ backend/views/request/index.php
@@ -10,54 +10,70 @@ use yii\grid\GridView;
 /** @var common\models\search\RequestSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Requests';
+$this->title = 'Murojaatlar';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="request-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['site/index']) ?>">Dashboard </a>
+                    </li>
+                    <li class="breadcrumb-item"><i class="feather-chevron-right"></i></li>
+                    <li class="breadcrumb-item active"><?= Html::encode($this->title) ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <h3><?= Html::encode($this->title) ?></h3>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-box">
+                <div class="table-responsive p-5">
+                    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
 
-    <p>
-        <?= Html::a('Create Request', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+//                            'id',
+                            'title',
+                            'first_name',
+                            'last_name',
+                            'phone_number',
+                            'comment:ntext',
+//                            'created_at',
+//                            'updated_at',
+                            [
+                                'class' => 'yii\grid\ActionColumn',
+                                'header' => 'Status',
+                                'headerOptions' => ['style' => 'text-align:center'],
+                                'template' => '{view}',
+                                'contentOptions' => ['style' => 'min-width:150px;max-width:150px;width:150px', 'class' => 'v-align-middle'],
+                                'buttons' => [
+                                    'view' => function ($url, $model) {
+                                        $url = '/request/view?id=' . $model->id;
+                                        if ($model->status == Request::STATUS_PENDING) {
+                                            $t = '/request/change-status?id=' . $model->id;
+                                            return Html::a('<span class="badge bg-danger" title="Отметить как прочитанные">New</span>', Url::to($t));
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+                                        }
+                                        return '<img src="/backend-files/img/ok.png" width="40px">
+                                                <a href="'.$url.'" class="btn btn-primary"><i class="far fa-eye"></i></a>
+';
+                                    },
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                                ],
 
-            'id',
-            'title',
-            'first_name',
-            'last_name',
-            'phone_number',
-            'comment:ntext',
-//            'created_at',
-//            'updated_at',
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'header' => 'Status',
-                'headerOptions' => ['style' => 'text-align:center'],
-                'template' => '{view}',
-                'contentOptions' => ['style' => 'min-width:150px;max-width:150px;width:150px', 'class' => 'v-align-middle'],
-                'buttons' => [
-                    'view' => function ($url, $model) {
-                        if ($model->status == Request::STATUS_PENDING) {
-                            $t = '/request/change-status?id=' . $model->id;
-                            return Html::a('<span class="badge bg-danger" title="Отметить как прочитанные">New</span>', Url::to($t));
+                            ],
+                        ],
+                    ]); ?>
 
-                        }
-                        return '<img src="/backend-files/img/ok.png" width="40px">';
-                    },
-
-                ],
-
-            ],
-        ],
-    ]); ?>
-
-
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- backend/views/request/view.php
+++ backend/views/request/view.php
@@ -12,33 +12,37 @@ $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="request-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'first_name',
-            'last_name',
-            'phone_number',
-            'comment:ntext',
-            'created_at',
-            'updated_at',
-            'status',
-        ],
-    ]) ?>
-
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['site/index']) ?>">Dashboard </a>
+                    </li>
+                    <li class="breadcrumb-item"><i class="feather-chevron-right"></i></li>
+                    <li class="breadcrumb-item active"><?= Html::encode($this->title) ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <h3><?= Html::encode($this->title ) ?></h3>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-box">
+                <?= DetailView::widget([
+                    'model' => $model,
+                    'attributes' => [
+                        'id',
+                        'title',
+                        'first_name',
+                        'last_name',
+                        'phone_number',
+                        'comment:ntext',
+                        'created_at',
+//                        'updated_at',
+                        'status',
+                    ],
+                ]) ?>
+            </div>
+        </div>
+    </div>
 </div>

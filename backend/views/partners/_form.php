375f7c206e9d37f5987682f21f0ea08dec3af32e
javokhirergashev <iqtidorziyosi@gmail.com>
2023-07-05 20:29:15 +0500

    partners

--- backend/views/partners/_form.php
+++ backend/views/partners/_form.php
@@ -13,54 +13,72 @@ use yii\widgets\ActiveForm;
         <?php $form = ActiveForm::begin(); ?>
         <div class="col-md-12">
             <div class="card-box">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <?php $languages = ['uz', 'en', 'ru'] ?>
+
+                    <?php foreach ($languages as $key => $language): ?>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link <?= $key != 0 ? ' ' : 'active' ?>"
+                                    id="<?= $language ?>-tab"
+                                    data-bs-toggle="tab"
+                                    data-bs-target="#<?= $language ?>" type="button" role="tab"
+                                    aria-controls="<?= $language ?>"
+                                    aria-selected="true"><?= $language ?>
+                            </button>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
                 <form action="#">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group row">
                                 <div class="form-group row">
-                                    <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                        <?php $languages = ['uz', 'en', 'ru'] ?>
-
-                                        <?php foreach ($languages as $key => $language): ?>
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link <?= $key != 0 ? ' ' : 'active' ?>"
-                                                        id="<?= $language ?>-tab"
-                                                        data-bs-toggle="tab"
-                                                        data-bs-target="#<?= $language ?>" type="button" role="tab"
-                                                        aria-controls="<?= $language ?>"
-                                                        aria-selected="true"><?= $language ?>
-                                                </button>
-                                            </li>
-                                        <?php endforeach; ?>
-                                    </ul>
                                     <div class="tab-content" id="myTabContent">
-                                        <?php foreach ($languages as $key => $language): ?>
-
+                                        <?php foreach ($languages
+                                                       as $key => $language): ?>
                                             <div class="tab-pane <?= $key != 0 ? ' ' : 'show active' ?>"
                                                  id="<?= $language ?>" role="tabpanel"
                                                  aria-labelledby="<?= $language ?>-tab">
-                                                <label class="col-form-label">Nomi</label>
-                                                <?= $form->field($model, 'name[' . $language . ']')->textInput(['maxlength' => true])->label(false) ?>
+                                                <div class="col-md-12">
+                                                    <div class="form-group row"
+                                                         style="margin-right: 0px!important; margin-bottom: 0px!important;">
+                                                        <label class="col-form-label">Nomi</label>
+                                                        <div class="col-md-9" style="margin-right: 0px!important;">
+                                                            <?= $form->field($model, 'name[' . $language . ']')->textInput(['maxlength' => true])->label(false) ?>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         <?php endforeach; ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group row">
-                                    <label class="col-form-label">Rasm</label>
-                                    <div class="col-md-9">
-                                        <?= $form->field($model, 'image')->fileInput(['accept' => 'image/*', 'class' => 'form-control'])->label(false) ?>
-                                    </div>
+                        <div class="col-md-6 mt-2">
+                            <div class="form-group row">
+                                <label class="col-form-label">Rasm</label>
+                                <div class="col-md-9">
+                                    <?= $form->field($model, 'image')->fileInput(['accept' => 'image/*', 'class' => 'form-control'])->label(false) ?>
                                 </div>
                             </div>
+                        </div>
+                        <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group row">
                                     <label class="col-form-label">Link</label>
                                     <div class="col-md-9">
-                                        <?= $form->field($model, 'link')->textInput(['maxlength' => true])->label(false) ?>
+                                        <?= $form->field($model, 'link')->textInput()->label(false) ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group row" style="padding-top: 20px!important;">
+                                    <label class="col-form-label">Statusi</label>
+                                    <div class="col-md-9">
+                                        <?= $form->field($model, 'status')->dropDownList([
+                                            \common\models\Position::STATUS_ACTIVE => "Active",
+                                            \common\models\Position::STATUS_INACTIVE => "InActive",
+                                        ], ['prompt' => "Statusni tanlang"])->label(false) ?>
                                     </div>
                                 </div>
                             </div>
@@ -74,20 +92,4 @@ use yii\widgets\ActiveForm;
             <?php ActiveForm::end(); ?>
         </div>
     </div>
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'name')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'link')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>

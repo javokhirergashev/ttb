a91378d1085d2a84c4755d9f5ec38c2a910c582b
javokhirergashev <iqtidorziyosi@gmail.com>
2023-07-05 13:44:57 +0500

    partners

--- backend/views/partners/_form.php
+++ backend/views/partners/_form.php
@@ -9,7 +9,71 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="partners-form">
+    <div class="row">
+        <?php $form = ActiveForm::begin(); ?>
+        <div class="col-md-12">
+            <div class="card-box">
+                <form action="#">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <div class="form-group row">
+                                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                        <?php $languages = ['uz', 'en', 'ru'] ?>
 
+                                        <?php foreach ($languages as $key => $language): ?>
+                                            <li class="nav-item" role="presentation">
+                                                <button class="nav-link <?= $key != 0 ? ' ' : 'active' ?>"
+                                                        id="<?= $language ?>-tab"
+                                                        data-bs-toggle="tab"
+                                                        data-bs-target="#<?= $language ?>" type="button" role="tab"
+                                                        aria-controls="<?= $language ?>"
+                                                        aria-selected="true"><?= $language ?>
+                                                </button>
+                                            </li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                    <div class="tab-content" id="myTabContent">
+                                        <?php foreach ($languages as $key => $language): ?>
+
+                                            <div class="tab-pane <?= $key != 0 ? ' ' : 'show active' ?>"
+                                                 id="<?= $language ?>" role="tabpanel"
+                                                 aria-labelledby="<?= $language ?>-tab">
+                                                <label class="col-form-label">Nomi</label>
+                                                <?= $form->field($model, 'name[' . $language . ']')->textInput(['maxlength' => true])->label(false) ?>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-form-label">Rasm</label>
+                                    <div class="col-md-9">
+                                        <?= $form->field($model, 'image')->fileInput(['accept' => 'image/*', 'class' => 'form-control'])->label(false) ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-form-label">Link</label>
+                                    <div class="col-md-9">
+                                        <?= $form->field($model, 'link')->textInput(['maxlength' => true])->label(false) ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <?= Html::submitButton('Saqlash', ['class' => 'btn btn-success']) ?>
+                            </div>
+
+                        </div>
+                </form>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'name')->textarea(['rows' => 6]) ?>
--- backend/views/partners/index.php
+++ backend/views/partners/index.php
@@ -10,38 +10,71 @@ use yii\grid\GridView;
 /** @var common\models\search\PartnersSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Partners';
+$this->title = 'Hamkorlar';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="partners-index">
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['/']) ?>">Dashboard </a>
+                    </li>
+                    <li class="breadcrumb-item"><i class="feather-chevron-right"></i></li>
+                    <li class="breadcrumb-item active"><?= Html::encode($this->title) ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h3><?= Html::encode($this->title) ?></h3>
 
     <p>
-        <?= Html::a('Create Partners', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Yangi hamkor yaratish', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-box">
+                <div class="table-responsive p-5">
+                    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+//                          'id',
+                            'name:ntext',
+                            'image',
+                            'link',
+                            'status',
+                            [
+                                'class' => 'yii\grid\ActionColumn',
+                                'header' => 'Amallar',
+                                'headerOptions' => ['style' => 'text-align:center'],
+                                'template' => '{buttons}',
+                                'contentOptions' => ['style' => 'min-width:150px;max-width:150px;width:150px', 'class' => 'v-align-middle'],
+                                'buttons' => [
+                                    'buttons' => function ($url, $model) {
+                                        $controller = Yii::$app->controller->id;
+                                        $code = <<<BUTTONS
+                                <div class="btn-group flex-center">
+                                    <a href="/{$controller}/update?id={$model->id}" class="btn btn-primary"><i class="far fa-edit"></i></a>
+                                    <a href="/{$controller}/delete?id={$model->id}" id="{$controller}{$model->id}" data-postID="{$model->id}" data-postType="{$controller}" class="btn btn-danger postRemove" data-method="post"><i class="far fa-trash-alt"></i></a>
+                                </div>
+BUTTONS;
+                                        return $code;
+                                    }
 
-//            'id',
-            'name:ntext',
-            'image',
-            'link',
-            'status',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Partners $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
+                                ],
 
+                            ],
+                        ],
+                    ]); ?>
 
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

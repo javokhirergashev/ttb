3a1e24de778023e8e19f9e1298e3b4f2b548e154
javokhirergashev <iqtidorziyosi@gmail.com>
2023-07-21 15:30:21 +0500

    diagnoz crud

--- backend/views/diagnosis-list/create.php
+++ backend/views/diagnosis-list/create.php
@@ -5,13 +5,24 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\DiagnosisList $model */
 
-$this->title = 'Create Diagnosis List';
+$this->title = 'Yangi tashhis qo\'shish';
 $this->params['breadcrumbs'][] = ['label' => 'Diagnosis Lists', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="diagnosis-list-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['site/index']) ?>">Dashboard </a></li>
+                    <li class="breadcrumb-item"><i class="feather-chevron-right"></i></li>
+                    <li class="breadcrumb-item active"><?= Html::encode($this->title) ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <h3><?= Html::encode($this->title) ?></h3>
 
     <?= $this->render('_form', [
         'model' => $model,

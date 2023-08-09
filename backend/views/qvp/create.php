9242b2debf5014f2b6107e8f43603476da12ea9e
sardoraviat <sardoraviat96@mail.ru>
2023-08-09 20:31:31 +0500

    bredcrumplar tugadi

--- backend/views/qvp/create.php
+++ backend/views/qvp/create.php
@@ -5,13 +5,25 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\Qvp $model */
 
-$this->title = 'Create Qvp';
+$this->title = "Yangi QVP qo'shish";
 $this->params['breadcrumbs'][] = ['label' => 'Qvps', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="qvp-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="page-header">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['site/index']) ?>">Dashboard </a></li>
+                    <li class="breadcrumb-item"><i class="feather-chevron-right"></i></li>
+                    <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['qvp/index']) ?>">QVP </a></li>
+                    <li class="breadcrumb-item"><i class="feather-chevron-right"></i></li>
+                    <li class="breadcrumb-item active"><?= Html::encode($this->title) ?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
     <?= $this->render('_form', [
         'model' => $model,

5b3b57e02e3fa65a09ed94f04f54aa6471248e55
javokhirergashev <iqtidorziyosi@gmail.com>
2023-09-02 17:39:17 +0500

    fix

--- backend/config/params.php
+++ backend/config/params.php
@@ -4,3 +4,48 @@ return [
     'bsVersion'  => '5.x',
 //    'bsDependencyEnabled' => false
 ];
+
+/**
+ *      1.TTB mudiri
+ *  Hamma narsa
+ *
+ *      2.Adminga
+ *  Hamma narsa
+ *
+ *      3. Statist
+ *
+ *   Aholiga bog'liq bulgan ma'lumotlarni kura oladi, update/delete lar bulmaydi
+ *
+ *     4. HR
+ *   Hodimlarga tegishli hamma narsalarni ko'rish, update/delete lari
+ *
+ *     5. UASH
+ *
+ *   Faqat o'zini uchastkasiga tegishli aholini barcha ma'lumotlarini ko'rish,
+ *   yaratish, orderga jo'natish, natijasini ko'rish, emlashini
+ *   faqat ko'rish(emlashni bajara olmasin), laborotoriyani ham ko'rish,
+ *   spetsialistlarni xulosasini faqat ko'rish (update yo'q),
+ *
+ *      5. Patronaj hamshira
+ *
+ *   Faqat o'zini uchastkasiga tegishli aholini barcha ma'lumotlarini ko'rish (), yangi aholi qo'sha olsin, update/delete qila olsin
+ *    orderni natijasini ko'rish(jo'natish yo'q), emlashini
+ *   faqat ko'rish(emlashni bajara olmasin), pdfini chiqarib berish, laborotoriyani ham ko'rish,
+ *   spetsialistlarni xulosasini faqat ko'rish (update yo'q),
+ *
+ *      5. Spetsialistlar
+ *
+ *   Tumandagi barcha UASHlar tomonidan yo'naltirgan aholiga xulosa yoza olsin, orderga yunaltirsin,
+ *   orderni natijasini ko'rish(jo'natish yo'q),  laborotoriyani ham ko'rish,
+ *  aholi listga o'tib diagnoz yoza olsin
+ *
+ *      6. Ginekologlar
+ *
+ *  Faqat homiladorlarni ro'yxatini ko'radi, xomiladorlar tarixini yaratadi
+ *
+ *
+ *      7. Ginekologlar
+ *
+ *  Faqat homiladorlarni ro'yxatini ko'radi, xomiladorlar tarixini yaratadi
+ *
+ */
